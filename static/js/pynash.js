75c53b2ec971c546f8eaacb9f392fbd8c7e7e9e2
Bill Israel <bill.israel@gmail.com>
2019-02-20 21:59:53 -0600

    Minor improvements and needless JS changes

--- static/js/pynash.js
+++ static/js/pynash.js
@@ -1,9 +1,9 @@
-let MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
     'August', 'September', 'October', 'November', 'December'];
-let DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 function amOrPM(hour) {
-    am = true;
+    let am = true;
     if (hour >= 12) {
         am = false;
         hour = hour - 12;
@@ -11,40 +11,48 @@ function amOrPM(hour) {
     return `${hour}:00${(am) ? 'am' : 'pm'}`;
 }
 
+function d(tag, content, children, attributes) {
+    let t = document.createElement(tag);
+
+    if (attributes !== undefined) {
+        for (var key in attributes) {
+            t[key] = attributes[key];
+        }
+    }
+
+    for (var idx in children) {
+        t.appendChild(children[idx]);
+    }
+
+    if (content.length) {
+        t.appendChild(document.createTextNode(content));
+    }
+    return t;
+}
+
 function meetupInfo(meetups) {
-    let meetup = meetups.data[0];
+    const meetup = meetups.data[0];
     let nextEvent = document.getElementById('next-event');
 
-    let meetup_link = document.createElement('a');
-    meetup_link.href = meetup.link;
-    meetup_link.appendChild(document.createTextNode(meetup.name));
+    let name = d('h3', '', [
+        d('a', meetup.name, [], {'href': meetup.link})
+    ]);
 
-    let name = document.createElement('h3');
-    name.appendChild(meetup_link);
+    let when_dt = new Date(meetup.time);
+    let day_and_time = `${DAYS[when_dt.getDay()]}, ${MONTHS[when_dt.getMonth()]} ${when_dt.getDate()}, ${when_dt.getFullYear()} @ ${amOrPM(when_dt.getHours())}`;
+    let when = d('p', day_and_time, [
+        d('strong', 'When? ', [])
+    ]);
 
-    let when = document.createElement('p');
-    let when_label = document.createElement('strong');
-    when_label.appendChild(document.createTextNode('When? '));
-    when_dt = new Date(meetup.time);
+    let venue_link = `${meetup.venue.name}, ${meetup.venue.address_1}`;
+    let venue_url = `https://www.google.com/maps/search/${venue_link}`;
+    let venue = d('p', '', [
+        d('strong', 'Where will we be? ', []),
+        d('a', venue_link, [], {'href': venue_url})
+    ]);
 
-    when.appendChild(when_label);
-    when.appendChild(document.createTextNode(`${DAYS[when_dt.getDay()]}, ${MONTHS[when_dt.getMonth()]} ${when_dt.getDate()}, ${when_dt.getFullYear()} @ ${amOrPM(when_dt.getHours())}`));
-
-
-    let venue_label = document.createElement('strong');
-    venue_label.appendChild(document.createTextNode('Where will we be? '));
-
-    let venue_link = document.createElement('a');
-    venue_link.href = `https://www.google.com/maps/search/${meetup.venue.name}, ${meetup.venue.address_1}`
-    venue_link.appendChild(document.createTextNode(`${meetup.venue.name}, ${meetup.venue.address_1}`));
-
-    let venue = document.createElement('p');
-    venue.appendChild(venue_label);
-    venue.appendChild(venue_link);
-
-    let description = document.createElement('div')
+    let description = d('div', '', []);
     description.innerHTML = meetup.description;
-
 
     nextEvent.appendChild(name);
     nextEvent.appendChild(when);

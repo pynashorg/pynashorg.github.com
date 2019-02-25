a693fae1d538bcf9a836e3b9fae6e9ea48de7bd8
Bill Israel <bill.israel@gmail.com>
2019-02-24 19:44:08 -0600

    Fix incorrect day of week in meetup details

--- static/js/pynash.js
+++ static/js/pynash.js
@@ -1,6 +1,6 @@
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
     'August', 'September', 'October', 'November', 'December'];
-const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 function amOrPM(hour) {
     let am = true;
